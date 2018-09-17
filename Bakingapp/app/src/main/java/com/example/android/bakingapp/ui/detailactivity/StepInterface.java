d9f5e8d14cb9bdf12ec97576d4961cf2cff5845b
Martin Zamarski <zamarskimartin@gmail.com>
2018-09-17 09:36:09 +0200

    Delete StepInterface.java

--- Bakingapp/app/src/main/java/com/example/android/bakingapp/ui/detailactivity/StepInterface.java
+++
@@ -1,8 +0,0 @@
-package com.example.android.bakingapp.ui.detailactivity;
-
-import com.example.android.bakingapp.data.Step;
-
-public interface StepInterface {
-    void onStepClick(Step step);
-
-}
